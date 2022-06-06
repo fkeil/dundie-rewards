6af77056c776523dce9d09c2ae8286316ac1ae58
Fernando Keil <keil.fernando@gmail.com>
2022-06-06 21:15:58 +0200

    __main__ using argparse to arguments

--- dundie/__main__.py
+++ dundie/__main__.py
@@ -1,4 +1,40 @@
-print("executing entrypoint for dundie ...")
+import argparse
+from turtle import st
+
+def load(filepath):
+    """Loads data from filepath to the database"""
+    try:
+        with open(filepath) as file_:
+            for line in file_:
+                print(line)
+    except FileNotFoundError as e:
+        print(f"File not found {e}")
 
 def main():
-    print("Executing dundie from entrypoint")
+    
+    parser = argparse.ArgumentParser(
+        description="Dunder Mifflin Rewars CLI",
+        epilog = "Enjoy and use with cautious.",
+    )
+    parser.add_argument(
+        "subcommand",
+        type = str,
+        help = "The subcommand to run",
+        choices = ("load", "show", "send"),
+        default = "help",
+    )
+    parser.add_argument(
+        "filepath",
+        type = str,
+        help = "File path to load",
+        default = None,
+    )
+    args = parser.parse_args()
+    
+    try:
+        globals()[args.subcommand](args.filepath)
+    except KeyError:
+        print("Subcommand is invalid.")
+
+if __name__ == "__main__":
+    main()
