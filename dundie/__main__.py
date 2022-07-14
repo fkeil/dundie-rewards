d5eed4b9d60a0007898fd161cccb95a569af55d0
Fernando Keil <keil.fernando@gmail.com>
2022-07-14 08:58:00 +0000

    reiniciando o projeto

--- dundie/__main__.py
+++ dundie/__main__.py
@@ -1,40 +1,4 @@
-import argparse
-from turtle import st
-
-def load(filepath):
-    """Loads data from filepath to the database"""
-    try:
-        with open(filepath) as file_:
-            for line in file_:
-                print(line)
-    except FileNotFoundError as e:
-        print(f"File not found {e}")
-
-def main():
-    
-    parser = argparse.ArgumentParser(
-        description="Dunder Mifflin Rewars CLI",
-        epilog = "Enjoy and use with cautious.",
-    )
-    parser.add_argument(
-        "subcommand",
-        type = str,
-        help = "The subcommand to run",
-        choices = ("load", "show", "send"),
-        default = "help",
-    )
-    parser.add_argument(
-        "filepath",
-        type = str,
-        help = "File path to load",
-        default = None,
-    )
-    args = parser.parse_args()
-    
-    try:
-        globals()[args.subcommand](args.filepath)
-    except KeyError:
-        print("Subcommand is invalid.")
+from dundie.cli import main
 
 if __name__ == "__main__":
     main()
