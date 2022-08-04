46ad599a4e5fab907da66aa281d7b74eb9a7aed6
Fernando Keil <keil.fernando@gmail.com>
2022-08-04 11:23:50 +0000

    Making an error

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-.PHONY: install virtualenv ipython clean test watch
+.PHONY: install virtualenv ipython clean test testci watch
 
 install:
 	@echo "Hello installing for dev environment"
@@ -12,6 +12,9 @@ ipython:
 
 test:
 	@.venv/bin/pytest -vv -s
+
+testci:
+	@.venv/bin/pytest -v --junitxml=test-result.xml
 
 watch:
 	@.venv/bin/ptw
--- tests/test_load.py
+++ tests/test_load.py
@@ -4,7 +4,13 @@ from .constants import PEOPLE_FILE
 
 @pytest.mark.unit
 @pytest.mark.high
-def test_load():
+def test_load_positive_has_2_people(request): # se colocar create_new_file dentro dos parenteses, apenas esta ffuncao vai usar as novas pastas de teste
     """"Test function load function"""
     assert len(load(PEOPLE_FILE)) == 2
-    assert load(PEOPLE_FILE)[0][0] == 'J'
+
+
+@pytest.mark.unit
+@pytest.mark.high
+def test_load_positive_first_name_starts_with_j(request): # se colocar create_new_file dentro dos parenteses, apenas esta ffuncao vai usar as novas pastas de teste
+    """"Test function load function"""
+    assert len(load(PEOPLE_FILE)) == 2
