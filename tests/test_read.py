e5c9e1a65c53319f4913c105cc8a9374afe834dc
Fernando Keil <fernandokeil@Fernandos-MacBook-Pro.local>
2022-09-12 18:31:01 +0200

    FOII

--- tests/test_read.py
+++ tests/test_read.py
@@ -1,32 +1,65 @@
 import pytest
 
-from dundie.core import read
-from dundie.database import add_person, commit, connect
+from dundie.core import load, read
+from dundie.database import get_session
+from dundie.models import Person
+from dundie.utils.db import add_person
+
+from .constants import PEOPLE_FILE
 
 
 @pytest.mark.unit
 def test_read_with_query():
-    db = connect()
+    session = get_session()
 
-    pk = "joe@doe.com"
-    data = {"name": "Joe Doe", "role": "Salesman", "dept": "Sales"}
-    _, created = add_person(db, pk, data)
+    data = {
+        "role": "Salesman",
+        "dept": "Sales",
+        "name": "Joe Doe",
+        "email": "joe@doe.com",
+    }
+    _, created = add_person(session, Person(**data))
     assert created is True
-    commit(db)
 
-    pk = "jim@doe.com"
-    data = {"name": "Jimmy Doe", "role": "Manager", "dept": "Management"}
-    _, created = add_person(db, pk, data)
+    data = {
+        "role": "Manager",
+        "dept": "Management",
+        "name": "Jim Doe",
+        "email": "jim@doe.com",
+    }
+    _, created = add_person(session, Person(**data))
     assert created is True
-    commit(db)
+
+    session.commit()
 
     response = read()
     assert len(response) == 2
 
     response = read(dept="Management")
     assert len(response) == 1
-    assert response[0]["name"] == "Jimmy Doe"
+    assert response[0]["name"] == "Jim Doe"
 
     response = read(email="joe@doe.com")
     assert len(response) == 1
     assert response[0]["name"] == "Joe Doe"
+
+
+@pytest.mark.unit
+def test_read_all_data():
+    load(PEOPLE_FILE)
+    result = read()
+    assert len(result) == 2
+
+
+@pytest.mark.unit
+def test_read_only_one_dept():
+    load(PEOPLE_FILE)
+    result = read(dept="Sales")
+    assert len(result) == 2
+
+
+@pytest.mark.unit
+def test_read_only_one_person():
+    load(PEOPLE_FILE)
+    result = read(email="jim@dundlermifflin.com")
+    assert len(result) == 1
